4ca0159a6d8c163c95134b78fe9d95ea41a15dcc
Maikoke <mamadoumaikoke11@gmail.com>
2023-03-06 09:34:53 +0100

    fixed hover effect

--- main.js
+++ main.js
@@ -2,16 +2,29 @@ const img = document.querySelectorAll('.project img');
 const viewProjects = document.querySelectorAll('.project .view-project');
 
 
+// for(let i = 0; i < viewProjects.length; i++){
+
+
+// img[i].addEventListener('mouseover', () => {
+//   viewProjects[i].classList.add('show');
+// });
+
+// img[i].addEventListener('mouseout', () => {
+//   viewProjects[i].classList.remove('show');
+// });
+
+// }
+
+
 for(let i = 0; i < viewProjects.length; i++){
 
 
-img[i].addEventListener('mouseover', () => {
-  viewProjects[i].classList.add('show');
-  console.log("hhh")
-});
-
-img[i].addEventListener('mouseout', () => {
-  viewProjects[i].classList.remove('show');
-});
+  img[i].addEventListener('mouseover', () => {
+    viewProjects[i].style.opacity = 1;
+  });
+  
+  img[i].addEventListener('mouseout', () => {
+    viewProjects[i].style.opacity = 0;
+  });
 
 }
